5c7f74c6c21eeb807b6f4a4697a310e6e0c6da3e
WilfriedW9 <zalo.Timothe@gmail.com>
2024-04-16 17:55:28 +0200

    Création du service de la DB

--- client/index.js
+++ client/index.js
@@ -1,10 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(){
-    loadHTMLTable([])
-})
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:4006/getAll")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  loadHTMLTable([]);
+});
 
 function loadHTMLTable(data) {
-
-    if (data.length === 0) {
-        console.log("No data is available")
-    }
+  if (data.length === 0) {
+    console.log("No data is available");
+  }
 }
---
+++ server/app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// create
+app.post("/insert", (req, res) => {
+
+})
+
+
+
+
+
+
+// read
+app.get("/getAll", (request, response) => {
+    console.log("test");
+    response.json({
+        success: true
+    })
+})
+
+
+
+
+// update
+
+
+
+
+
+
+// delete
+
+
+app.listen(process.env.PORT, () => console.log(`app is running on port http://localhost:${process.env.PORT}`))
