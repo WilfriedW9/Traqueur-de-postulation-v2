0825e87adc73e0c3842f21425253b4c40ba21e06
WilfriedW9 <zalo.Timothe@gmail.com>
2024-04-16 18:22:05 +0200

    Pb connection a la DB

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
+import dbService from "./dbService"
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
